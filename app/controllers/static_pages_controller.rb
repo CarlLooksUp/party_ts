ce0852dac02ec7c084ece412eb512f32193397b6
Carl Morrissey <carl.morrissey@gmail.com>
2014-06-03 04:16:11 -0400

    non-interactive order table done

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -12,6 +12,14 @@ class StaticPagesController < ApplicationController
   end
 
   def order
+    @beers = ["IPA", "Blonde/Summer", "Pale Ale", "White Ale", "Saison", 
+              "Porter", "Stout", "Hefeweizen", "Lager", "Belgium"]
+    @breweries = ["Slumbrew", "Pretty Things", "Jack's Abbey", "Clown Shoes",
+                  "CBC", "Cisco", "Sam Adams", "BBC"]
+    @dares = ["Steal a glass", "Sip a stranger's drink", "Take a shot",
+              "Ask a patron to use the bathroom", "Introduce two strangers",
+              "Coax a round out of a stranger"]
+    @boxes = @beers.clone.concat(@breweries).concat(@dares).shuffle.insert(12, "FREE")
   end
 
   def contact
